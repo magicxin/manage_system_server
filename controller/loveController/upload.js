50e8c6bb4f111a400f7277dbda60b25d2f2a8e37
MagixSui <suixin@jcinfo.online>
2019-09-27 14:06:50 +0800

    fix mkdir

--- controller/loveController/upload.js
+++ controller/loveController/upload.js
@@ -2,7 +2,7 @@ const Router = require("koa-router");
 const router = new Router();
 var koaBody = require("koa-body");
 const path = require('path');
-const { getUploadDirName, getUploadFileExt, mkdirAsync, checkMimeType, handleFileName } = require('../../utils');
+const { getUploadDirName, mkdirSync, checkMimeType, handleFileName } = require('../../utils');
 
 let middleware = {
   multipart:true, // 支持文件上传
@@ -15,13 +15,17 @@ let middleware = {
       // 获取文件后缀
       // const ext =getUploadFileExt(file.name);
       const mimetype = checkMimeType(file.type);
-      console.log(__dirname)
       // 最终要保存到的文件夹目录
+      // const dir = `public/uploads/${mimetype}/${getUploadDirName()}`; // 这样也可以
       const dir = path.join(__dirname,`../../public/uploads/${mimetype}/${getUploadDirName()}`);
       // 检查文件夹是否存在如果不存在则新建文件夹
-      mkdirAsync(dir, () => {});
-      // 重新覆盖 file.path 属性
+      mkdirSync(dir);
       file.path = `${dir}/${handleFileName(file.name)}`;
+      // mkdirAsync(dir, () => {
+      //   // 重新覆盖 file.path 属性
+      //   file.path = `${dir}/${handleFileName(file.name)}`;
+      //   console.log(file.path)
+      // });
     },
   }
 };
